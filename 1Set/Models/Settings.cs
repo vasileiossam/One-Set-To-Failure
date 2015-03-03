41c4ea0b3f0e0609a323c1b7ef638aa816d58e84
vsam <jacarandabill@gmail.com>
2015-03-03 11:10:44 +1100

    fixes

--- 1Set/Models/Settings.cs
+++ 1Set/Models/Settings.cs
@@ -4,15 +4,27 @@ using System.Globalization;
 namespace Set.Models
 {
 	public class Settings
-	{
-		public int? _isMetric;
+    {
+        #region Defaults
+        const int DefaultIsMetric = RegionInfo.CurrentRegion.IsMetric ? 1 : 0;
+        const int DefaultRepsToAdvance = 12;
+        const int DefaultStartingReps = 5;
+        
+        // 1 min 30secs
+        const int DefaultRestTimerId = 4;
+        
+        // +1
+        const int DefaultWorkoutGoalId = 1;
+        #endregion
+
+        public int? _isMetric;
 		public int IsMetric 
 		{ 
 			get 
 			{ 
 				if (_isMetric == null) 
 				{
-					_isMetric = RegionInfo.CurrentRegion.IsMetric ? 1 : 0;
+                    _isMetric = DefaultIsMetric;
 				}
 				return (int) _isMetric;
 			} 
@@ -21,6 +33,46 @@ namespace Set.Models
 			}
 		}
 
+        #region Settings that can be overriden in Exercises 
+        
+        // total reps to advance to next weight
+        // values 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18...50
+        public int? _repsToAdvance;
+        public int RepsToAdvance
+        {
+            get
+            {
+                if (_repsToAdvance == null)
+                {
+                    _repsToAdvance = DefaultRepsToAdvance;
+                }
+                return (int)_repsToAdvance;
+            }
+            set
+            {
+                _repsToAdvance = value;
+            }
+        }
+        
+        // reps when starting training on a weight for the first time
+        // values 0..50
+        public int? _startingReps;
+        public int StartingReps
+        {
+            get
+            {
+                if (_startingReps == null)
+                {
+                    _startingReps = DefaultStartingReps;
+                }
+                return (int)_startingReps;
+            }
+            set
+            {
+                _startingReps = value;
+            }
+        }
+        
 
         public RestTimer _restTimer;
         public RestTimer RestTimer
@@ -29,7 +81,7 @@ namespace Set.Models
             {
                 if (_restTimer == null)
                 {
-                    _restTimer = new RestTimer() { Seconds = 60 };
+                    _restTimer = new RestTimer() { Seconds = 60 }; <- iid
                 }
                 return _restTimer;
             }
@@ -38,10 +90,10 @@ namespace Set.Models
                 _restTimer = value;
             }
         }
+        #endregion
 
-		public Settings ()
+        public Settings ()
 		{
-
 		}
 	}
 }
--- 1Set/Pages/SettingsPage.xaml.cs
+++ 1Set/Pages/SettingsPage.xaml.cs
@@ -19,11 +19,7 @@ namespace Set
         // Training Rules 
         // (you can override in Exercises) 
         //
-        // Training rules are ON / OFF
-        //
-        // Total reps to advance to next weight:                       12 <- combo with values  0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18...50
-        // Reps when starting training on a weight for the first time:  5 <- combo with values 0..50
-        
+       
         // Workout goal in Reps
         // (number of Reps to increase performance in Reps since the last workout or how many more Reps aim to do each time)
         //    +1
@@ -31,23 +27,6 @@ namespace Set
         //    +1 every 2 times
         //    +1 every 3 times
         //    +1 every 4 times
-
-        // Rest timer 
-        //  hide rest timer
-        //  30 secs
-        //  45 secs
-        //  1 min
-        //  1 min 30 secs
-        //  1 min 45 secs
-        //  2 min
-        //  2 min 30 secs
-        
-        // Exercises page
-        // have options to override all training rules (rest timer included)
-        // where to have it? see UI patterns 
-        //   app toolbar button: Training Rules?
-        //   button Training Rules?
-        //   tabs: Basic / Advanced?
 	}
 }
 
