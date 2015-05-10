ab63fd98982460752f8442dffab581515ee1cbaa
vsam <jacarandabill@gmail.com>
2015-05-10 20:42:08 +1000

    Quotes assets

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -177,19 +177,94 @@ namespace Set.Droid
 			public const int nosound = 2130837537;
 			
 			// aapt resource value: 0x7f020022
-			public const int sound = 2130837538;
+			public const int Quotes_1 = 2130837538;
 			
 			// aapt resource value: 0x7f020023
-			public const int splash = 2130837539;
+			public const int Quotes_10 = 2130837539;
 			
 			// aapt resource value: 0x7f020024
-			public const int SplashScreen = 2130837540;
+			public const int Quotes_11 = 2130837540;
 			
 			// aapt resource value: 0x7f020025
-			public const int success = 2130837541;
+			public const int Quotes_12 = 2130837541;
 			
 			// aapt resource value: 0x7f020026
-			public const int warning = 2130837542;
+			public const int Quotes_13 = 2130837542;
+			
+			// aapt resource value: 0x7f020027
+			public const int Quotes_14 = 2130837543;
+			
+			// aapt resource value: 0x7f020028
+			public const int Quotes_15 = 2130837544;
+			
+			// aapt resource value: 0x7f020029
+			public const int Quotes_16 = 2130837545;
+			
+			// aapt resource value: 0x7f02002a
+			public const int Quotes_17 = 2130837546;
+			
+			// aapt resource value: 0x7f02002b
+			public const int Quotes_18 = 2130837547;
+			
+			// aapt resource value: 0x7f02002c
+			public const int Quotes_19 = 2130837548;
+			
+			// aapt resource value: 0x7f02002d
+			public const int Quotes_2 = 2130837549;
+			
+			// aapt resource value: 0x7f02002e
+			public const int Quotes_20 = 2130837550;
+			
+			// aapt resource value: 0x7f02002f
+			public const int Quotes_21 = 2130837551;
+			
+			// aapt resource value: 0x7f020030
+			public const int Quotes_22 = 2130837552;
+			
+			// aapt resource value: 0x7f020031
+			public const int Quotes_23 = 2130837553;
+			
+			// aapt resource value: 0x7f020032
+			public const int Quotes_24 = 2130837554;
+			
+			// aapt resource value: 0x7f020033
+			public const int Quotes_25 = 2130837555;
+			
+			// aapt resource value: 0x7f020034
+			public const int Quotes_3 = 2130837556;
+			
+			// aapt resource value: 0x7f020035
+			public const int Quotes_4 = 2130837557;
+			
+			// aapt resource value: 0x7f020036
+			public const int Quotes_5 = 2130837558;
+			
+			// aapt resource value: 0x7f020037
+			public const int Quotes_6 = 2130837559;
+			
+			// aapt resource value: 0x7f020038
+			public const int Quotes_7 = 2130837560;
+			
+			// aapt resource value: 0x7f020039
+			public const int Quotes_8 = 2130837561;
+			
+			// aapt resource value: 0x7f02003a
+			public const int Quotes_9 = 2130837562;
+			
+			// aapt resource value: 0x7f02003b
+			public const int sound = 2130837563;
+			
+			// aapt resource value: 0x7f02003c
+			public const int splash = 2130837564;
+			
+			// aapt resource value: 0x7f02003d
+			public const int SplashScreen = 2130837565;
+			
+			// aapt resource value: 0x7f02003e
+			public const int success = 2130837566;
+			
+			// aapt resource value: 0x7f02003f
+			public const int warning = 2130837567;
 			
 			static Drawable()
 			{
