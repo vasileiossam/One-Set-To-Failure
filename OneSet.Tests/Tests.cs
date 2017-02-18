414b4eb491611bbaf665eda38b4b0b63f8766200
vas <vas@samaltanos.com>
2017-02-19 09:55:18 +1100

    Added test for automapper

---
+++ OneSet.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OneSet.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test_AutomapperConfig()
+        {
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Automapper();
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
