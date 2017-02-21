2985c8b462e5f9921efaa53a330bcee9b0be9eb3
vas <vas@samaltanos.com>
2017-02-21 19:43:17 +1100

    Added UITest Project

--- OneSet/Views/WorkoutPage.xaml.cs
+++ OneSet/Views/WorkoutPage.xaml.cs
@@ -19,31 +19,6 @@ namespace OneSet.Views
 			await ViewModel.LoadAsync ();
 			BindingContext = ViewModel;
         }
-
-	    void OnTextChanged(object sender, TextChangedEventArgs  e)
-		{
-			// fires multiple times and can go to infinite loop
-			// example: when in the workout list we have a workout with weight 500.1
-			// when we will tap this workout app will crash
-
-//			var entry = sender as Entry;
-//			if (entry == null) return;
-//			var value = entry.Text;
-//
-//			// max length = 4
-//			if(value.Length > 4)
-//			{
-//				// Remove Last character 
-//				value = value.Remove(value.Length - 1);
-//
-//				// https://forums.xamarin.com/discussion/44018/removing-and-adding-of-textchanged-event-handler
-//				entry.TextChanged -= OnTextChanged;
-//				entry.Text = value; 
-//				Task.Yield();
-//				entry.TextChanged += OnTextChanged;
-//
-//			}
-		}
 	}
 }
 
