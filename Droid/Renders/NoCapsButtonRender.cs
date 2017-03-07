c66a348c4653ff29a0030e48c95b90913ccc8e4e
vas <vas@samaltanos.com>
2017-03-07 19:35:54 +1100

    Added NoCapsButton

---
+++ Droid/Renders/NoCapsButtonRender.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using OneSet.Droid.Renders;
+using OneSet.Controls;
+
+[assembly: ExportRenderer (typeof (NoCapsButton), typeof (NoCapsButtonRender))]
+
+namespace OneSet.Droid.Renders
+{
+	public class NoCapsButtonRender : ButtonRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Button> e)
+		{
+			base.OnElementChanged (e);
+            var button = Control;
+            button.SetAllCaps(false);
+        }
+
+	}
+}
+
